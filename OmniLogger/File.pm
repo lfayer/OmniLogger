de2382d970f82dc6974776a98421441197261117
Leon Fayer <leonf25@yahoo.com>
2014-04-30 13:25:32 -0400

    adding timestamp to file logging

--- OmniLogger/File.pm
+++ OmniLogger/File.pm
@@ -1,20 +1,22 @@
 package OmniLogger::File;
 use base 'OmniLogger';
 use Filehandle;
+use POSIX qw( strftime );
  
 my $_fh;
  
 sub init {
-    my $self = shift;
-    $_fh = FileHandle->new($self->{config}->{'OmniLogger::File'}->{filename},"a+");
+  my $self = shift;
+  $_fh = FileHandle->new($self->{config}->{'OmniLogger::File'}->{filename},"a+");
+  $self->SUPER::init(@_);
 }
  
 sub log {
-    my $self = shift;
-    my $msg = shift;
-    if (defined $_fh) {
-        $_fh->printf("%s","$msg\n");
-    }
+  my $self = shift;
+  my $msg = shift;
+  if (defined $_fh) {
+    $_fh->printf("%s","[".strftime("%Y-%m-%d %H:%M:%S", localtime(time))."] $msg\n");
+  }
 }
  
 sub DESTROY { $_fh->close; }
